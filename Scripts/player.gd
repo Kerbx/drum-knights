527ae2d1399450859f619c3160d909d92edbf40b
Adam Nobody <uliya.ru3456@gmail.com>
2023-07-09 20:27:35 +0300

    Фикс скорости движения персонажа

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-@export var MAX_SPEED = 300
-@export var ACCELERATION = 1500
-@export var FRICTION = 1200
+const MAX_SPEED = 100
+const ACCELERATION = 500
+const FRICTION = 600
 
 @onready var axis = Vector2.ZERO
 
