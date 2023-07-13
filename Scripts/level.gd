c25a8083f3d8224cbcd737323a1dbc12535970de
Kerbx <r3ostat@ya.ru>
2023-07-14 00:37:10 +0300

    fixed spawn player.

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,17 +1,16 @@
 extends Node
 
-var playerScene = preload("res://Scenes/player.tscn")
 
+var playerScene = preload("res://Scenes/player.tscn")
 
 
 func _enter_tree():
 	var player = playerScene.instantiate()
-	for child in get_parent().get_children():
+	for child in get_children():
 		if child.name == "spawnPoint":
 			child.add_child(player)
 			print(child.name)
 			break
-
 
 
 func _process(delta):
